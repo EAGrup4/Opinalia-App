9154a5c5e3d90ddcd7be31a9fac6bae883168f0b
guillemmunozferran <guillemeetac@gmail.com>
2018-01-02 17:51:35 +0100

    Add ratings to products

--- src/pages/item-create/item-create.ts
+++ src/pages/item-create/item-create.ts
@@ -12,16 +12,20 @@ export class ItemCreatePage {
   @ViewChild('fileInput') fileInput;
 
   isReadyToSave: boolean;
-
+  rating: { title: string, comment: string, rate: string} = {
+    title: '',
+    comment: '',
+    rate:''
+  };
   item: any;
 
   form: FormGroup;
 
   constructor(public navCtrl: NavController, public viewCtrl: ViewController, formBuilder: FormBuilder, public camera: Camera) {
     this.form = formBuilder.group({
-      profilePic: [''],
-      name: ['', Validators.required],
-      about: ['']
+      title:'',
+      comment: '',
+      rate:''
     });
 
     // Watch the form for changes, and
@@ -33,36 +37,8 @@ export class ItemCreatePage {
   ionViewDidLoad() {
 
   }
-
-  getPicture() {
-    if (Camera['installed']()) {
-      this.camera.getPicture({
-        destinationType: this.camera.DestinationType.DATA_URL,
-        targetWidth: 96,
-        targetHeight: 96
-      }).then((data) => {
-        this.form.patchValue({ 'profilePic': 'data:image/jpg;base64,' + data });
-      }, (err) => {
-        alert('Unable to take photo');
-      })
-    } else {
-      this.fileInput.nativeElement.click();
-    }
-  }
-
-  processWebImage(event) {
-    let reader = new FileReader();
-    reader.onload = (readerEvent) => {
-
-      let imageData = (readerEvent.target as any).result;
-      this.form.patchValue({ 'profilePic': imageData });
-    };
-
-    reader.readAsDataURL(event.target.files[0]);
-  }
-
-  getProfileImageStyle() {
-    return 'url(' + this.form.controls['profilePic'].value + ')'
+  createitem(){
+    console.log(this.item.name);
   }
 
   /**
--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, ModalController, NavParams } from 'ionic-angular';
 
 import { Items } from '../../providers/providers';
 
@@ -11,9 +11,18 @@ import { Items } from '../../providers/providers';
 export class ItemDetailPage {
   item: any;
 
-  constructor(public navCtrl: NavController, navParams: NavParams, items: Items) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, navParams: NavParams, items: Items) {
     this.item = navParams.get('item') //|| items.defaultItem;
     console.log(this.item.ratings)
+  }
+
+  addRating() {
+    let addModal = this.modalCtrl.create('ItemCreatePage');
+    addModal.onDidDismiss(item => {
+      if (item) {
+      }
+    })
+    addModal.present();
   }
 
 }
