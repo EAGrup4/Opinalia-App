65816267bdd944fd0070f2ab6e93d683f69154e0
guillemmunozferran <guillemeetac@gmail.com>
2018-01-19 20:32:45 +0100

    All/laptop/desktop/tablet/phone/acc finished

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -13,7 +13,12 @@ export class SearchPage {
 
 
   rankings: any = [];
-   allbest: any=[{}];
+  allbest: any=[{}];
+  desktopbest: any=[{}];
+  laptopbest: any=[{}];
+  tabletbest: any=[{}];
+  phonebest: any=[{}];
+  accessoriesbest: any=[{}];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) {
     this.rankings="all"
@@ -39,11 +44,65 @@ export class SearchPage {
    * Navigate to the detail page for this item.
    */
   charge(){
-    let seq=this.items.query();
+    let seq1=this.items.allbest();
+    let seq2=this.items.desktopbest();
+    let seq3=this.items.laptopbest();
+    let seq4=this.items.tabletbest();
+    let seq5=this.items.phonebest();
+    let seq6=this.items.accessoriesbest();
 
-    seq.subscribe((res: any) => {
+    seq1.subscribe((res: any) => {
       this.allbest=res;
-      console.log(this.allbest)
+      if (res.status == 'success') {
+
+      } else {
+
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+    seq2.subscribe((res: any) => {
+      this.desktopbest=res;
+      if (res.status == 'success') {
+
+      } else {
+
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+    seq3.subscribe((res: any) => {
+      this.laptopbest=res;
+      if (res.status == 'success') {
+
+      } else {
+
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+    seq4.subscribe((res: any) => {
+      this.tabletbest=res;
+      if (res.status == 'success') {
+
+      } else {
+
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+    seq5.subscribe((res: any) => {
+      this.phonebest=res;
+      if (res.status == 'success') {
+
+      } else {
+
+      }
+    }, err => {
+      console.error('ERROR', err);
+    });
+    seq6.subscribe((res: any) => {
+      this.accessoriesbest=res;
       if (res.status == 'success') {
 
       } else {
--- src/providers/items/items.ts
+++ src/providers/items/items.ts
@@ -12,20 +12,30 @@ export class Items {
   query() {
     let seq =this.api.get('products/all')
     return seq;
-
-/*
-    seq.subscribe((res: any) => {
-      this.items=res;
-      if (res.status == 'success') {
-
-      } else {
-
-      }
-    }, err => {
-      console.error('ERROR', err);
-    });
-  console.log("return")
-    return this.items;*/
+  }
+  allbest(){
+    let seq =this.api.get('products/best7')
+    return seq;
+  }
+  desktopbest(){
+    let seq =this.api.get('products/category/best7/desktop')
+    return seq;
+  }
+  laptopbest(){
+    let seq =this.api.get('products/category/best7/laptop')
+    return seq;
+  }
+  tabletbest(){
+    let seq =this.api.get('products/category/best7/tablet')
+    return seq;
+  }
+  phonebest(){
+    let seq =this.api.get('products/category/best7/phone')
+    return seq;
+  }
+  accessoriesbest(){
+    let seq =this.api.get('products/category/best7/accessories')
+    return seq;
   }
 
   add(item: Item) {
